c480f394deab7076bdf3e76449ac7064b3a41760
S Saikumar <saidrling@Mac-88.lan>
2025-09-09 11:27:43 +0530

    used for checking API

--- day-33/main.py
+++ day-33/main.py
@@ -1,1 +1,15 @@
-print("welcome to Mac Family")
+import requests
+
+response = requests.get(url="https://api.wheretheiss.at/v1/satellites/25544")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["longitude"]
+latitude = data["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
+
+print(response.json())
